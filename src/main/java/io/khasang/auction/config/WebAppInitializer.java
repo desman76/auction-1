b44a233fa47b966d5ad49535d9fb24d8ed642551
skuznet <azon.sk@gmail.com>
2018-11-20 21:06:28 +0300

    add basic proj structure

---
+++ src/main/java/io/khasang/auction/config/WebAppInitializer.java
@@ -0,0 +1,22 @@
+package io.khasang.auction.config;
+
+import io.khasang.auction.config.application.WebConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+// web.xml
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
