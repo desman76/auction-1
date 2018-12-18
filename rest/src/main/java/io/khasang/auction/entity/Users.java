2f37cebd0d423c77eb77c1284ceb1402d5117fa0
kisin <wolf_joker@inbox.ru>
2018-12-18 21:59:51 +0300

    Add 'Users' entity

---
+++ rest/src/main/java/io/khasang/auction/entity/Users.java
@@ -0,0 +1,78 @@
+package io.khasang.auction.entity;
+
+import org.hibernate.type.BigIntegerType;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(precision=19)
+    private BigIntegerType id;
+
+    private String name;
+    private String email;
+    private String surname;
+    private String middle_name;
+    private String role;
+    private String nickname;
+
+    public BigIntegerType getId() {
+        return id;
+    }
+
+    public void setId(BigIntegerType id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
